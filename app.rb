cb5eb16cbde17ed940960df9b2a2096fd7e94a70
Ciprianna <ciprianna17@gmail.com>
2015-07-09 23:08:23 -0500

    Views show up correctly. Database set up.  Now need to create a   view to add questions (maybe?) and populate the database. Then   change index html to pull from database.

--- app.rb
+++ app.rb
@@ -8,8 +8,8 @@ require_relative "database_class_methods.rb"
 require_relative "database_instance_methods.rb"
 
 # Models
-require_relative "models/quesitons.rb"
+require_relative "models/questions.rb"
 require_relative "models/choices.rb"
 
 # Controllers
-require_relative "controllers/question.rb"
+require_relative "controllers/main.rb"
--- database_setup.rb
+++ database_setup.rb
@@ -3,7 +3,7 @@ DATABASE = SQLite3::Database.new("german.db")
 
 # Creates the table
 DATABASE.execute("CREATE TABLE IF NOT EXISTS questions (id INTEGER PRIMARY KEY, vocabulary TEXT NOT NULL, answer INTEGER NOT NULL);")
-DATABASE.execute("CREATE TABLE IF NOT EXISTS choices (id INTEGER PRIMARY KEY, choice TEXT NOT NULL question_id INTEGER NOT NULL);")
+DATABASE.execute("CREATE TABLE IF NOT EXISTS choices (id INTEGER PRIMARY KEY, choice TEXT NOT NULL, question_id INTEGER NOT NULL);")
 
 # Returns the results as a Hash
 DATABASE.results_as_hash = true
